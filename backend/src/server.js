8e8bd5c079ba34bfaa91d7044bf62bf44b4f7fe6
Jose Rosas <josejrrosas@yahoo.com>
2025-08-11 20:35:37 -0500

    created connection to mongo db and created schema and model  for note

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,21 @@
 //node framework
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
 
+//to properly use the .env
+dotenv.config();
+const PORT = process.env.PORT || 5001
 //Express application instance, web server object for routes & middleware
 const app = express();
 
 app.use("/api/notes", notesRoutes);
 
+connectDB()
+
 //tells app to start accepting connections
-app.listen(5001, () => {
-  console.log("Server started on port 5001.");
+app.listen(PORT, () => {
+  console.log("Server started on port:", PORT);
 });
+
