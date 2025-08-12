f52624b5206f61c10ad43f91b0daa2943f60c52e
Jose Rosas <josejrrosas@yahoo.com>
2025-08-11 21:01:40 -0500

    added logic to getAllNotes controller to retrieve notes from Note model

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,17 +1,22 @@
+import Note from "../models/Note.js"
 
-
-export const getAllNotes = (req, res) => {
-  res.status(200).send("You just fetched the notes.");
-}
+export const getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find()
+    res.status(200).json(notes)
+  } catch (error) {
+    console.error("Error in getAllNotes controller", error)
+    res.status(500).json({message: "Internal Server Error"})
+  }
+};
 
 export const createNote = (req, res) => {
   res.status(201).send("note created successfully.");
-}
+};
 
 export const updateNote = (req, res) => {
   res.status(200).send("note updated successfully.");
-
-}
+};
 export const deleteNote = (req, res) => {
   res.status(200).send("note deleted successfully.");
-}
+};
