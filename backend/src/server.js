c007c896845e26b0877f272f948e175490c4a5d4
Jose Rosas <josejrrosas@yahoo.com>
2025-08-13 17:11:00 -0500

    added middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,20 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 
 //middleware
+// app.use(...) Tells Express: “Run this function for every incoming request, 
+// no matter the path or method (GET, POST, etc.).”
+// express.json It’s like a translator at the door — 
+// it takes the JSON from the request body, turns it into a JS object, 
+// and hands it to your route handler so you can use it directly.
 app.use(express.json());
+
+//express middleware function
+//next tells express: 
+// "im done with this middleware - move on to the next matching route or middleware"
+app.use((req, res, next) => {
+  console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+  next();
+});
 
 app.use("/api/notes", notesRoutes);
 
